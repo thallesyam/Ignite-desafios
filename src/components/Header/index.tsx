5b7c823d3d38783819a43b7a17f7d015e6490ea4
Thalles Ian <thallesyam@users.=.github.com>
2021-06-15 22:12:18 -0300

    create header and show prismic posts in console

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,16 @@
+import Link from 'next/link';
+
+import styles from './header.module.scss';
+import commonStyles from '../../styles/common.module.scss';
+
 export default function Header() {
-  // TODO
+  return (
+    <header className={`${styles.header} ${commonStyles.container}`}>
+      <Link href="/">
+        <a>
+          <img src="/images/logo.svg" alt="logo" />
+        </a>
+      </Link>
+    </header>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
 import { GetStaticProps } from 'next';
+import { format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 
+import Prismic from '@prismicio/client';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
+
 import styles from './home.module.scss';
+
+import { FiCalendar, FiUser } from 'react-icons/fi';
+
+import Header from '../components/Header';
 
 interface Post {
   uid?: string;
@@ -24,13 +32,86 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ results }) {
+  console.log(results);
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+  return (
+    <>
+      <Header />
 
-//   // TODO
-// };
+      <section className={`${commonStyles.container} ${styles.postsContainer}`}>
+        <div className={styles.post}>
+          <h1>Como utilizar Hooks</h1>
+          <p>Pensando em sincronização em vez de ciclos de vida.</p>
+
+          <div>
+            <p>
+              <FiCalendar />
+              <span>15 Mar 2021</span>
+            </p>
+            <p>
+              <FiUser />
+              <span>Joseph Oliveira</span>
+            </p>
+          </div>
+        </div>
+        <div className={styles.post}>
+          <h1>Como utilizar Hooks</h1>
+          <p>Pensando em sincronização em vez de ciclos de vida.</p>
+
+          <div>
+            <p>
+              <FiCalendar />
+              <span>15 Mar 2021</span>
+            </p>
+            <p>
+              <FiUser />
+              <span>Joseph Oliveira</span>
+            </p>
+          </div>
+        </div>
+        <div className={styles.post}>
+          <h1>Como utilizar Hooks</h1>
+          <p>Pensando em sincronização em vez de ciclos de vida.</p>
+
+          <div>
+            <p>
+              <FiCalendar />
+              <span>15 Mar 2021</span>
+            </p>
+            <p>
+              <FiUser />
+              <span>Joseph Oliveira</span>
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const { results } = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'post')],
+    {}
+  );
+
+  // const posts = results.map(post => {
+  //   return {
+  //     id: post.id,
+  //     title: post.data.title,
+  //     subtitle: post.data.subtitle,
+  //     author: post.data.author,
+  //     createdAt: format(new Date(post.first_publication_date), 'DD MMMM YYYY', {
+  //       locale: pt,
+  //     }),
+  //   };
+  // });
+
+  // TODO FORMAT DATE
+
+  return {
+    props: { results },
+  };
+};
