0118e7c9e9c56223a38c386b13c4be01d96060f5
Thalles Ian <thallesyam@users.=.github.com>
2021-06-17 23:49:06 -0300

    create pagination and format date

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import { GetStaticProps } from 'next';
+import { useState } from 'react';
+
 import { format } from 'date-fns';
-import pt from 'date-fns/locale/pt';
+import ptBR from 'date-fns/locale/pt-BR';
 
 import Prismic from '@prismicio/client';
 import { getPrismicClient } from '../services/prismic';
@@ -12,6 +14,7 @@ import styles from './home.module.scss';
 import { FiCalendar, FiUser } from 'react-icons/fi';
 
 import Header from '../components/Header';
+import Link from 'next/link';
 
 interface Post {
   uid?: string;
@@ -32,59 +35,82 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home({ results }) {
-  console.log(results);
+const options = {
+  year: 'numeric',
+  month: 'short',
+  day: 'numeric',
+  timeZone: 'UTC',
+  dateStyle: 'medium',
+};
+
+function FormatPosts(posts: PostPagination): Post[] {
+  const newPostsFormatted = posts.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: new Intl.DateTimeFormat('pt-BR', options).format(
+        new Date(post.first_publication_date)
+      ),
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+    };
+  });
+
+  return newPostsFormatted;
+}
+
+export default function Home({ finalProps }) {
+  const { results, next_page } = finalProps;
+
+  const [posts, setPosts] = useState<Post[]>(results);
+  const [nextPage, setNextPage] = useState(next_page);
+
+  async function getMorePosts(): Promise<void> {
+    if (!next_page) {
+      return;
+    }
+    const response = await fetch(next_page);
+    const nextPost = await response.json();
+
+    const nextPostFormatted = FormatPosts(nextPost);
+
+    setPosts([...posts, ...nextPostFormatted]);
+    setNextPage(nextPost.nextPage);
+  }
 
   return (
     <>
       <Header />
 
       <section className={`${commonStyles.container} ${styles.postsContainer}`}>
-        <div className={styles.post}>
-          <h1>Como utilizar Hooks</h1>
-          <p>Pensando em sincronização em vez de ciclos de vida.</p>
+        {posts.map(post => (
+          <Link key={post.uid} href={`/posts/${post.uid}`}>
+            <a>
+              <div className={styles.post}>
+                <h1>{post.data.title}</h1>
+                <p>{post.data.subtitle}</p>
+                <div>
+                  <p>
+                    <FiCalendar />
+                    <span>{post.first_publication_date}</span>
+                  </p>
+                  <p>
+                    <FiUser />
+                    <span>{post.data.author}</span>
+                  </p>
+                </div>
+              </div>
+            </a>
+          </Link>
+        ))}
 
-          <div>
-            <p>
-              <FiCalendar />
-              <span>15 Mar 2021</span>
-            </p>
-            <p>
-              <FiUser />
-              <span>Joseph Oliveira</span>
-            </p>
-          </div>
-        </div>
-        <div className={styles.post}>
-          <h1>Como utilizar Hooks</h1>
-          <p>Pensando em sincronização em vez de ciclos de vida.</p>
-
-          <div>
-            <p>
-              <FiCalendar />
-              <span>15 Mar 2021</span>
-            </p>
-            <p>
-              <FiUser />
-              <span>Joseph Oliveira</span>
-            </p>
-          </div>
-        </div>
-        <div className={styles.post}>
-          <h1>Como utilizar Hooks</h1>
-          <p>Pensando em sincronização em vez de ciclos de vida.</p>
-
-          <div>
-            <p>
-              <FiCalendar />
-              <span>15 Mar 2021</span>
-            </p>
-            <p>
-              <FiUser />
-              <span>Joseph Oliveira</span>
-            </p>
-          </div>
-        </div>
+        {nextPage && (
+          <a className={styles.buttonGetPosts} onClick={getMorePosts}>
+            Clique para exibir
+          </a>
+        )}
       </section>
     </>
   );
@@ -92,26 +118,36 @@ export default function Home({ results }) {
 
 export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient();
-  const { results } = await prismic.query(
+  const response = await prismic.query(
     [Prismic.Predicates.at('document.type', 'post')],
-    {}
+    {
+      pageSize: 1,
+    }
   );
 
-  // const posts = results.map(post => {
-  //   return {
-  //     id: post.id,
-  //     title: post.data.title,
-  //     subtitle: post.data.subtitle,
-  //     author: post.data.author,
-  //     createdAt: format(new Date(post.first_publication_date), 'DD MMMM YYYY', {
-  //       locale: pt,
-  //     }),
-  //   };
-  // });
+  const posts = response.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: new Intl.DateTimeFormat('pt-BR', options).format(
+        new Date(post.first_publication_date)
+      ),
+      data: {
+        title: post.data.title as string,
+        subtitle: post.data.subtitle as string,
+        author: post.data.author as string,
+      },
+    };
+  });
 
-  // TODO FORMAT DATE
+  const { next_page } = response;
+
+  const finalProps = {
+    next_page: next_page,
+    results: posts,
+  };
 
   return {
-    props: { results },
+    props: { finalProps },
+    revalidate: 60 * 60 * 24, // 24 horas
   };
 };
