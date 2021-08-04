d55de85c37c078464b8a12efdc6f54f4a5128be2
Thalles Ian <thallesyam@users.=.github.com>
2021-08-03 21:04:55 -0300

    feat: finish slide style component

--- src/components/SliderComponent.tsx
+++ src/components/SliderComponent.tsx
@@ -1,28 +1,69 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore, {
-  Navigation,Pagination,Mousewheel,Keyboard
-} from 'swiper/core';
+import { Flex, Heading, Text } from '@chakra-ui/react';
+import Link from 'next/link';
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper';
 
-import "swiper/swiper.min.css";
-import "swiper/components/navigation/navigation.min.css"
-import "swiper/components/pagination/pagination.min.css"
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-SwiperCore.use([Navigation,Pagination,Mousewheel,Keyboard]);
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y, Autoplay]);
 
 export const SliderComponent = () => {
   return (
-    <>
-      <Swiper cssMode={true} navigation={true} pagination={true} mousewheel={true} keyboard={true}>
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+    <Flex w="100%" h={["250px","450px"]} maxW="1240px" mx="auto" mb={["5","10"]}>
+      <Swiper
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        autoplay={{
+          delay:4000,
+        }}
+        style={{width: '100%', display: 'flex',flex: '1'}}
+      > 
+
+        <SwiperSlide key={1}>
+          <Flex
+            w="100%"
+            h={['250px', '450px']}
+            align="center"
+            justify="center"
+            direction="column"
+            bgImage={`url('/europePage.png')`}
+            bgPosition="100% 30%"
+            bgRepeat="no-repeat"
+            bgSize="cover"
+            textAlign="center"
+          >
+            <Link href={`/`}>
+              <a>
+                <Heading fontSize={["3xl","4xl","5xl"]} color="gray.100" fontWeight="bold">Europa</Heading>
+                <Text fontWeight="bold" color="gray.300" fontSize={["0.8rem","1xl", "2xl"]} mt={["2","4"]}>{' '}</Text>
+              </a>
+            </Link>
+          </Flex>
+        </SwiperSlide>
+        
+        <SwiperSlide key={1}>
+          <Flex
+            w="100%"
+            h={['250px', '450px']}
+            align="center"
+            justify="center"
+            direction="column"
+            bgImage={`url('/europePage.png')`}
+            bgPosition="100% 30%"
+            bgRepeat="no-repeat"
+            bgSize="cover"
+            textAlign="center"
+          >
+            <Link href={`/`}>
+              <a>
+                <Heading fontSize={["3xl","4xl","5xl"]} color="gray.100" fontWeight="bold">Europa</Heading>
+                <Text fontWeight="bold" color="gray.300" fontSize={["0.8rem","1xl", "2xl"]} mt={["2","4"]}>{' '}</Text>
+              </a>
+            </Link>
+          </Flex>
+        </SwiperSlide>
+
       </Swiper>
-    </>
+    </Flex>
   )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,13 @@ import { AppProps } from 'next/dist/next-server/lib/router/router'
 
 import { theme } from '../styles/theme'
 
+import 'swiper/swiper.min.css';
+import 'swiper/components/navigation/navigation.min.css';
+import 'swiper/components/pagination/pagination.min.css';
+import 'swiper/components/scrollbar/scrollbar.min.css';
+
+import '../styles/styles.css';
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
